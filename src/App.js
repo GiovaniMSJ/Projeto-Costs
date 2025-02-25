4a8e09f17e6ba987652e2133a4a321c328ffeaa2
GiovaniMSJ <140211502+GiovaniMSJ@users.noreply.github.com>
2025-02-25 19:21:34 -0300

    NavBar e Footer
    Foi criado o estilo da navbar e do footer do projeto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+// Importaçoes
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/Newproject';
 import Container from './components/layout/Container';
+import Projects from './components/pages/Projects';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
+// Pagina principal
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/company'>Contato</Link>
-        <Link to='/contact'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass='min-height'>
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/projects' element={<Projects />}></Route>
           <Route path='/company' element={<Company />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='/newproject' element={<NewProject />}></Route>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
